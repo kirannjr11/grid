389d494e2508b650cfad8fcfb2381a77362ce75b
kirannjr11 <ghalekiran03@gmail.com>
2026-02-21 20:57:57 +0900

    completed first video

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-gsap.to(".box", {
+var tl = gsap.timeline();
+
+tl.to(".box1", {
 x:1000,
 duration: 3,
-delay:2,
 backgroundColor: "white",
+height:"200px",
+width:"200px",
+rotate:360,
+borderRadius: "50%",
+scale:2,
+
+yoyo: true
+
 }) 
+
+tl.from(".box2", {
+x:1500,
+y: 500,
+duration: 3,
+backgroundColor: "white",
+
+}) 
+
+tl.from("h1", {
+    opacity: 0,
+    duration:1,
+    y:30,
+    stagger:1
+})
