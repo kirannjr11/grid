694f8c4f2c8596f7c669359e29a4885a971f1f3d
kirannjr11 <ghalekiran03@gmail.com>
2026-02-21 23:27:07 +0900

    completed scrollTriger

--- script.js
+++ script.js
@@ -1,44 +1,12 @@
-gsap.from(".page1 .box", {
-  scale: 2,
-  duration: 3,
-  delay: 2,
-
-  backgroundColor: "pink",
-  rotate: 360,
-  yoyo: true,
-  borderRadius: "50%",
-});
-
-gsap.from(".page2 .box", {
-  scale: 2,
-  duration: 3,
-
-  backgroundColor: "pink",
-  rotate: 360,
-  yoyo: true,
-  borderRadius: "80%",
-  scrollTrigger: {
-    trigger: ".page2 .box",
-    scroller: "body",
-    markers: true,
-    start: "top 50%",
-  },
-});
-
-gsap.from(".page3 .box", {
-  scale: 2,
-  duration: 3,
-  
-
-  backgroundColor: "pink",
-  rotate: 360,
-  yoyo: true,
-  borderRadius: "50%",
-
-  scrollTrigger: {
-    trigger: ".page3 .box",
-    scrolller: "body",
-    markers: "true",
-    start: "top 50%",
-  },
-});
+gsap.to(".page2 h1", {
+    xPercent: -54,
+    scrollTrigger: {
+        trigger: ".page2",
+        scroller: "body",
+        markers: true,
+        start: "top 0%",
+        end: "top -100%",
+        scrub: 2,
+        pin: true,
+    }
+})
